f140e7d05c03eda97d40ffe8a1cbd7a280aa1145
Justin Pritchard <jpritchard9518@gmail.com>
2019-06-30 17:35:03 -0500

    Basic Page Layout, Controllers, and Segues

---
+++ Rolocards/Rolocards/Controllers/HomeController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeControllerViewController.swift
+//  Rolocards
+//
+//  Created by Justin Pritchard on 6/30/19.
+//  Copyright © 2019 Justin Pritchard. All rights reserved.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
