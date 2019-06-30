e38c0fe46899d1294b8ec5be9e0ae1e14ca146a0
Justin Pritchard <jpritchard9518@gmail.com>
2019-06-30 17:23:56 -0500

    Initializing XCode Project

---
+++ Rolocards/Rolocards/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Rolocards
+//
+//  Created by Justin Pritchard on 6/30/19.
+//  Copyright © 2019 Justin Pritchard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
